6a5ebaeec8fde11fa29b5aa159431b390b0a57ef
solomonraza <solomonrazak99@gmsil.com>
2023-11-29 11:05:30 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { marked } from "marked";
+import React, { Component } from "react";
+import Badge from "react-bootstrap/Badge"; // badge for header styling.
+import Button from "react-bootstrap/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Dependencies installed.
+
+// npm install react-bootstrap bootstrap
+// npm install marked - Marked.js is the brains behind the conversion of markdown and is very simple to use.
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: "",
+    };
+  }
+
+  // updateMarkdown function
+  updateMarkDown = (markdown) => {
+    this.setState({ markdown });
+  }
+
+  
+
+  // clear textarea function
+  clearText = () => {
+    this.setState({ markdown: '' });
+  };
+
+  render() {
+    // object for inline style.
+    var inputStyle = {
+      width: "400px",
+      height: "50vh",
+      marginLeft: "auto",
+      marginRight: "auto",
+      padding: "10px",
+    };
+
+    var outputStyle = {
+      width: "400px",
+      height: "50vh",
+      backgroundColor: "#DCDCDC",
+      marginLeft: "auto",
+      marginRight: "auto",
+      padding: "10px",
+    };
+
+    return (
+      <div className="App">
+        <div className="container">
+          <h1 className="text-center p-2">
+            <Badge className="text-align-center mx-auto" variant="Light">
+              Markdown Previewer
+            </Badge>
+          </h1>
+          <div className="row mt-3" id="flex-container">
+            <div className="col-md-6" id="markown-input">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Markdown Input
+                  </Badge>
+                </h4>
+              </div>
+              <div className="mark-input">
+                <textarea
+                  className="input mx-auto"
+                  style={inputStyle}
+                  value={this.state.markdown}
+                  onChange={(e) => this.updateMarkDown(e.target.value)}
+                >
+                  {console.log(this.state.markdown)}
+                </textarea>
+                <div id="btnReset" className="d-flex justify-content-center">
+                  <Button variant="primary" onClick={this.clearText}>Reset</Button>{" "}
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6" id="preview-input">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Preview
+                  </Badge>
+                </h4>
+              </div>
+              <div className="mark-input">
+                <div
+                  className="input"
+                  style={outputStyle}
+                  dangerouslySetInnerHTML={{
+                    __html: marked(this.state.markdown),
+                  }}
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
